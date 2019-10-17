35c4dba00986855010ff63635926bbc7309fe12c
tamartwe <tamartwena@gmail.com>
2019-10-17 20:45:37 +0300

    more changes

---
+++ demo_eval_denial_of_service_eslint_plugin/index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
+var route = router.route("/");
+
+// Create endpoint /api/ for POSTS
+route.post(async (req, res) => {
+ 
+	var malicousExpression = req.body.malicousExpression;
+	eval(malicousExpression);
+	return res.json({"message" : "all is good"});
+});
+
+app.use("/api", router);
+
+app.listen(port);
