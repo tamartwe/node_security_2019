d54bccfe99eaad2cb19393ae7f5023dfd2b70398
tamartwe <tamartwena@gmail.com>
2019-10-17 22:37:55 +0300

    changes

---
+++ demo_command_injection/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const child_process = require('child_process');
+
+const  Person =  require('./models/persons.js');
+const app = express();
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+mongoose.connect('mongodb://localhost:27017/securityDemo');
+
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/', function (req, res) {
+  child_process.exec(
+    'gzip ' + req.query.file_path,
+    function (err, data) {
+      console.log('err: ', err)
+      console.log('data: ', data);
+    });
+  res.send('Zip Done')
+})
+
+app.listen(port);
