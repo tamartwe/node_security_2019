afa72daa25dc5fa3743bd9877e1124663716a994
tamartwe <tamartwena@gmail.com>
2019-10-21 18:18:04 +0200

    changes

--- demo_injection/index.js
+++ demo_injection/index.js
@@ -14,29 +14,22 @@ const port = process.env.PORT || 3000;
 
 
 //Signup API
-app.post('/signup', function (req, res) {
+app.post('/signup', async (req, res) => {
 
   const newUser = new User(req.body);
-  newUser.save(function(err,data){
-    if(err){
-        res.send(err);
-    }else{
-        res.send('User Registered');
-    }
-  })
+  await newUser.save();
+  res.send('User Registered');
+
 });
 
 //Login API
-app.post('/login',function(req,res){
-    User.findOne({'email': req.body.email,'password':req.body.password},function(err,data){
-        if(err){
-            res.send(err);
-        }else if(data){
-            res.send('User Login Successful');
-        }else {
-            res.send('Wrong Username Password Combination');
-        }
-    })
-});
+app.post('/login',async (req,res) => {
+    const data = await User.findOne({'email': req.body.email,'password':req.body.password});
+    if (data) {
+        res.send('User Login Successful');
+    } else {
+        res.send('Wrong Username Password Combination');
+      }
+    });
 
 app.listen(port);
